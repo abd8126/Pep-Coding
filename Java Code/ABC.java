5df676cd389cc193c3568386bc40e9abbb572d4b
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2021-02-12 17:03:03 +0530

    Practice

---
+++ Java Code/ABC.java
@@ -0,0 +1,10 @@
+public class ABC {
+    public void run(){
+        System.out.println("Hello");
+    }
+    public static void main(String []args){
+        System.out.println("Anubhav");
+        ABC pqr = new ABC();
+        pqr.run();
+    }
+}
---
+++ Java Code/day1.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+ Welcome to GDB Online.
+ GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+ C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+ Code, Compile, Run and Debug online from anywhere in world.
+
+ *******************************************************************************/
+class Main{
+    public void run()
+    {
+        System.out.println("Anubhav");
+
+    }
+    public void eat()
+    {
+        System.out.println("how! are you");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        Main abc=new Main();
+        abc.run();
+        abc.eat();
+    }
+}
