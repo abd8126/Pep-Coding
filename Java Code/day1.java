208b7b59de6d575cc3e2afd03b984f214d99e713
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2021-03-27 11:13:42 +0530

    Practice

--- Java Code/day1.java
+++ Java Code/day1.java
@@ -1,12 +1,4 @@
-/******************************************************************************
-
- Welcome to GDB Online.
- GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
- C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
- Code, Compile, Run and Debug online from anywhere in world.
-
- *******************************************************************************/
-class Main{
+class Animal{
     public void run()
     {
         System.out.println("Anubhav");
@@ -19,7 +11,7 @@ class Main{
 
     public static void main(String[] args) {
         System.out.println("Hi");
-        Main abc=new Main();
+        Animal abc= new Animal();
         abc.run();
         abc.eat();
     }
---
+++ Java Code/demo.java
@@ -0,0 +1,13 @@
+class demo {
+	void eat()
+	{
+		System.out.println("Hi1234 ");
+
+	}
+public static void main(String[] args)
+{
+	System.out.println("Hi ");
+	demo a1 = new demo ();
+	a1.eat();
+}
+}
