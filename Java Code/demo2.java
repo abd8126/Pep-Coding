9f4001a3e8a113e09bd41991e898b743b92dbc4c
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2021-03-27 12:06:31 +0530

    Practice

---
+++ Java Code/demo2.java
@@ -0,0 +1,12 @@
+//initializatio using refrece variale
+class demo2{
+    String name;
+    int age;
+    public  static void main(String[] args) {
+        demo2 a1 = new demo2();
+        a1.name = "Anubhav";
+        a1.age = 10;
+        System.out.println(a1.name+""+a1.age);
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 30daysofJava
+Pillars of Java:
+1. Object
+2. Class and Methods
+3. Polymorphism
+4. Astraction
+5. Inheritace
+6. 
 30 Days of Java with Code
